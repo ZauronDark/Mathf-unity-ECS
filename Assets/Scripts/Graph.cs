30b5307253eee3fd482f99530293507613445f09
SauronDark <SauronDark95@gmail.com>
2019-01-09 21:16:05 +0530

    updated to v1.1
    -fps now updates every 2 seconds (previously each frame)
    -fixed minor issues

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -41,7 +41,21 @@ public class Graph : MonoBehaviour
     private Transform[] holder;
     private float steps = 0f;
     private JobData jobData;
-
+    private float fpsSum;
+    private bool fpsClock = false;
+    private int fpscount;
+    private readonly string[] stringsFrom00To99 = {
+        "00", "01", "02", "03", "04", "05", "06", "07", "08", "09",
+        "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+        "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
+        "30", "31", "32", "33", "34", "35", "36", "37", "38", "39",
+        "40", "41", "42", "43", "44", "45", "46", "47", "48", "49",
+        "50", "51", "52", "53", "54", "55", "56", "57", "58", "59",
+        "60", "61", "62", "63", "64", "65", "66", "67", "68", "69",
+        "70", "71", "72", "73", "74", "75", "76", "77", "78", "79",
+        "80", "81", "82", "83", "84", "85", "86", "87", "88", "89",
+        "90", "91", "92", "93", "94", "95", "96", "97", "98", "99"
+    };
 
     private void Awake()
     {
@@ -127,8 +141,7 @@ public class Graph : MonoBehaviour
 
     private void Update()
     {
-        fpsNum.text = ((int)(1f / Time.unscaledDeltaTime)).ToString();
-        
+
         //calculations for Postions of each GameObjects
         xLoop = new XLoop()
         {
@@ -147,6 +160,20 @@ public class Graph : MonoBehaviour
         gOHandle = gOpos.Schedule(transforms, xLoopHandle);
 
         JobHandle.ScheduleBatchedJobs();
+
+        fpscount++;
+        fpsSum += 1f / Time.unscaledDeltaTime;
+        if ((int)Time.time % 2 == 0 && fpsClock)
+        {
+            fpsClock = false;
+            fpsNum.text = stringsFrom00To99[Mathf.Clamp((int)(fpsSum / fpscount), 0, 99)];
+            fpscount = 0;
+            fpsSum = 0f;
+        }
+        else if ((int)Time.time % 2 != 0 && !fpsClock)
+        {
+            fpsClock = true;
+        }
     }
 
     private void LateUpdate()
